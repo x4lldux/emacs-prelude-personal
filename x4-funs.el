291623cc7c084f14223fc18dc5d0f3f25ece700c
X4lldux <xalldux@gmail.com>
2012-11-03 19:11:36 +0100

    Fixing some functions.

--- x4-funs.el
+++ x4-funs.el
@@ -2,32 +2,27 @@
 (defun x4-smarter-beginning-of-line ()
   "Move point to first beginning-of-line or non-whitespace character or first non-whitespace after comment."
   (interactive "^")
-  (if (= (point) (line-beginning-position))
-      (let ((oldpos (point)))
-	(back-to-indentation)
-	(if (eq (point) oldpos)
-	    (progn
-	      (beginning-of-line)
-	      (if (eq (point) oldpos)
-		  (beginning-of-line-text)
-		nil)
-	      )
-	  (nil))
+  (let (
+	(oldpos (point))
+	(indentpos (progn
+		     (back-to-indentation)
+		     (point)
+		     )
+		   )
+	(textpos (progn
+		   (beginning-of-line-text)
+		   (point)
+		   )
+		 )
 	)
-    (let ((oldpos (point)))
-      (back-to-indentation)
-      (if (eq (point) oldpos)
-	  (progn
-	    (beginning-of-line-text)
-	    (if (eq (point) oldpos)
-		(beginning-of-line)
-	      nil)
-	    )
-	(beginning-of-line))
-      )
+    (cond
+     ((> oldpos textpos) (beginning-of-line-text))
+     ((and (<= oldpos textpos) (> oldpos indentpos))  (back-to-indentation))
+     ((and (<= oldpos indentpos) (> oldpos (line-beginning-position))) (beginning-of-line))
+     (t (beginning-of-line-text))
+     )
     )
   )
-
 
 ;; clear function for eshell
 (defun eshell/clear ()
@@ -69,11 +64,72 @@
 	   (set-window-start w1 s2)
 	   (set-window-start w2 s1)))))
 
-;; (defun x4-yank-primary-at-point ()
-;;   "Yank from primary selection at point"
-;;   (let (old-x-primary (x-select-enable-primary))
-;;     (setq x-select-enable-primary t)
-;;     (yank)
-;;     (setq x-select-enable-primary 'old-x-primary)
-;;     )
-;;   )
+(defun prelude-swap-windows ()
+  ;;(defun rotate-windows ()
+  "Rotate your windows"
+  (interactive)
+  (cond
+   ((not (> (count-windows) 1))
+    (message "You can't rotate a single window!"))
+   (t
+    (let ((num-windows (count-windows))
+	  (i (count-windows)))
+      (while  (> i 0)
+	(let* ((w1 (elt (window-list) i))
+	       (w2 (elt (window-list) (- (% i num-windows) 1)))
+	       (b1 (window-buffer w1))
+	       (b2 (window-buffer w2))
+	       (s1 (window-start w1))
+	       (s2 (window-start w2)))
+	  (set-window-buffer w1 b2)
+	  (set-window-buffer w2 b1)
+	  (set-window-start w1 s2)
+	  (set-window-start w2 s1)
+	  (setq i (1- i))))))))
+
+(defun prelude-swap-windows ()
+  "Rotate your windows"
+  (interactive)
+  (cond
+   ((not (> (count-windows) 1))
+    (message "You can't rotate a single window!"))
+   (t
+    (let ((i 1)
+	  (num-windows (count-windows)))
+      (while  (< i num-windows)
+	(let* ((w1 (elt (window-list) i))
+	       (w2 (elt (window-list) (+ (% i num-windows) 1)))
+	       (b1 (window-buffer w1))
+	       (b2 (window-buffer w2))
+	       (s1 (window-start w1))
+	       (s2 (window-start w2)))
+	  (set-window-buffer w1 b2)
+	  (set-window-buffer w2 b1)
+	  (set-window-start w1 s2)
+	  (set-window-start w2 s1)
+	  (setq i (1+ i))))))))
+
+(defun x4-yank-primary-at-point ()
+  "Yank from primary selection at point"
+  (interactive "^")
+  (let ((primary
+	 (cond
+	  ((eq system-type 'windows-nt)
+	   ;; MS-Windows emulates PRIMARY in x-get-selection, but not
+	   ;; in x-get-selection-value (the latter only accesses the
+	   ;; clipboard).  So try PRIMARY first, in case they selected
+	   ;; something with the mouse in the current Emacs session.
+	   (or (x-get-selection 'PRIMARY)
+	       (x-get-selection-value)))
+	  ((fboundp 'x-get-selection-value) ; MS-DOS and X.
+	   ;; On X, x-get-selection-value supports more formats and
+	   ;; encodings, so use it in preference to x-get-selection.
+	   (or (x-get-selection-value)
+	       (x-get-selection 'PRIMARY)))
+	  ;; FIXME: What about xterm-mouse-mode etc.?
+	  (t
+	   (x-get-selection 'PRIMARY)))))
+    (unless primary
+      (error "No selection is available"))
+    (push-mark (point))
+    (insert primary)))
